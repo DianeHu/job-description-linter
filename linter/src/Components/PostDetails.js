6f8b56879cfff2debebee12b4e29c7c67dc71bda
Diane Hu <dh224@duke.edu>
2018-07-24 19:00:22 -0400

    Working highlighting

--- linter/src/Components/PostDetails.js
+++ linter/src/Components/PostDetails.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import 'jquery';
-import 'highlight-within-textarea';
-//import 'jquery-highlighttextarea';
+import {VetoedWords} from "../Services/WordFinder";
 
 class PostDetails extends Component {
     constructor(props) {
         super(props);
+
+        this.applyHighlights = this.applyHighlights.bind(this);
     }
 
     applyHighlights(event) {
-        var text = event.target.value;
-        console.log("hit");
-        /*$('textarea').highlightTextarea({
-            words: ['Please']
-        });*/
-        $('.highlight').highlightWithinTextarea({
-            highlight: 'please' // string, regexp, array, function, or custom object
+        var termRay = [];
+
+        var highlightPairs = VetoedWords(event.target.value);
+        for(var i = 0; i < highlightPairs.length; i++){
+            termRay.push(highlightPairs[i].word);
+        }
+
+        var source = event.target.value;
+        var words = source.split(' ');
+        var output = '';
+        $.each(words, function(idx, word) {
+            if ($.inArray(word, termRay) >= 0) {
+                output += '<mark>' + word + '</mark> ';
+            } else {
+                output += word + ' ';
+            }
         });
+
+        document.getElementById("output").innerHTML = output;
+
     }
 
     render() {
@@ -28,6 +41,7 @@ class PostDetails extends Component {
                     <div className="form-group">
                         <textarea className="highlight form-control" onChange={this.applyHighlights} rows="5"></textarea>
                     </div>
+                    <div id="output" className="highlighted"></div>
                 </div>
             </div>
         );
@@ -35,89 +49,3 @@ class PostDetails extends Component {
 }
 
 export default PostDetails;
-
-/*import React, { Component } from 'react';
-import $ from 'jquery';
-import './Styling/PostDetails.css';
-
-class PostDetails extends Component {
-    constructor(props) {
-        super(props);
-
-        var uaTemp = window.navigator.userAgent.toLowerCase();
-        this.state = {
-            ua: uaTemp,
-            isIE: !!uaTemp.match(/msie|trident\/7|edge/),
-            isWinPhone: uaTemp.indexOf('windows phone') !== -1,
-            isIOS: uaTemp.indexOf('windows phone') !== -1 && !!uaTemp.match(/ipad|iphone|ipod/)
-        };
-
-        this.bindEvents();
-    }
-
-    applyHighlights(text) {
-        text = text
-            .replace(/\n$/g, '\n\n')
-            .replace(/[A-Z].*?\b/g, '<mark>$&</mark>');
-
-        if (this.state.isIE) {
-            // IE wraps whitespace differently in a div vs textarea, this fixes it
-            text = text.replace(/ /g, ' <wbr>');
-        }
-
-        return text;
-    }
-
-    handleInput(event) {
-        var text = event.target.value;
-        var highlightedText = this.applyHighlights(text);
-        $('.highlights').html(highlightedText);
-    }
-
-    handleScroll() {
-        var scrollTop = $('textarea').scrollTop();
-        $('.backdrop').scrollTop(scrollTop);
-
-        var scrollLeft = $('textarea').scrollLeft();
-        $('.backdrop').scrollLeft(scrollLeft);
-    }
-
-    fixIOS() {
-        $('.highlights').css({
-            'padding-left': '+=3px',
-            'padding-right': '+=3px'
-        });
-    }
-
-    bindEvents() {
-        $('textarea').on({
-            'input': this.handleInput,
-            'scroll': this.handleScroll
-        })
-
-        var uaTemp = window.navigator.userAgent.toLowerCase();
-        if(uaTemp.indexOf('windows phone') !== -1 && !!uaTemp.match(/ipad|iphone|ipod/)) {
-            this.fixIOS();
-        }
-    }
-
-    render() {
-        return(
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-header">Job description</div>
-                    <div class="form-group">
-                        <div class="container">
-                            <div class="backdrop">
-                                <div class="highlights"></div>
-                            </div>
-                            <textarea onChange="handleInput" class="form-control" rows="5"></textarea>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-export default PostDetails;*/
--- linter/src/Containers/FormContainer.js
+++ linter/src/Containers/FormContainer.js
@@ -5,36 +5,11 @@ import PostDetails from "../Components/PostDetails";
 class FormContainer extends Component {
     constructor(props) {
         super(props);
-
-        this.state = { posting : '' };
-
-        this.handleJobPostingChange = this.handleJobPostingChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleJobPostingChange(event) {
-        this.setState({posting: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        this.highlight();
-    }
-
-    highlight() {
-        var highlightPairs = VetoedWords(this.state.posting);
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-group" >
-                    <label htmlFor="jobPosting" ></label>
-                    <PostDetails></PostDetails>
-                    <br />
-                    <input className="btn btn-primary btn-lg" type="submit" value="Submit"/>
-                </div>
-            </form>
+            <PostDetails></PostDetails>
         )
     }
 }
