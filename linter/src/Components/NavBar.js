7e04b9eb106170aaaa92d659b873263254915348
Siyu Jiang <blackjackjiang@gmail.com>
2018-07-24 09:50:21 -0700

    added nav bar

--- linter/src/Components/NavBar.js
+++ linter/src/Components/NavBar.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import './NavBar.css';
 
 class NavBar extends Component {
-    handleClick = () => {
-        this.props.clickHandler(this.textArea);
-    };
-
     render() {
         return (
-            <div className="NavBar">
-                <button onClick={this.handleClick}>{this.props.name}</button>
-            </div>
+            <nav className="nav nav-pills nav-fill">
+                <a className="nav-item nav-link active" href="#">Home</a>
+                <a className="nav-item nav-link disabled" href="#">Explore</a>
+                <a className="nav-item nav-link disabled" href="#">History (soon to come!)</a>
+                <a className="nav-item nav-link disabled" href="#">Suggestions (soom to come!)</a>
+
+            </nav>
         )
     }
 }
+
+export default NavBar;
