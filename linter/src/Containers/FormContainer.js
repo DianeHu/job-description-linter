3c33129a8e13b44ec6a67c89e706e2efde90cd0f
Diane Hu <dh224@duke.edu>
2018-07-24 15:17:17 -0400

    Nonworking highlighting, trying jquery plugin

--- linter/src/Containers/FormContainer.js
+++ linter/src/Containers/FormContainer.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import {VetoedWords} from "../Services/WordFinder";
 
 class FormContainer extends Component {
     constructor(props) {
         super(props);
+
+        this.state = { posting : '' };
 
         this.handleJobPostingChange = this.handleJobPostingChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleJobPostingChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({posting: event.target.value});
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        this.highlight();
+    }
+
+    highlight() {
+        var highlightPairs = VetoedWords(this.state.posting);
     }
 
     render() {
@@ -21,10 +29,8 @@ class FormContainer extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group" >
                     <label htmlFor="jobPosting" ></label>
-                    <textarea className="form-control" id="jobPosting" value={this.props.name} onChange={this.handleJobPostingChange} rows="15"/>
-
+                    <textarea className="form-control" id="jobPosting" rows="15"/>
                     <br />
-
                     <input className="btn btn-primary btn-lg" type="submit" value="Submit"/>
                 </div>
             </form>
