38f486b99eb094a3317953177f7f58f09b2c868a
Diane Hu <dh224@duke.edu>
2018-07-24 16:52:20 -0400

    Jquery not found

--- linter/src/Components/PostDetails.js
+++ linter/src/Components/PostDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import 'highlight-within-textarea';
 
 class PostDetails extends Component {
     constructor(props) {
@@ -9,18 +10,18 @@ class PostDetails extends Component {
     applyHighlights(event) {
         var text = event.target.value;
         console.log("hit");
-        $('.my-textarea').highlightWithinTextarea({
+        $('.highlight').highlightWithinTextarea({
             highlight: 'please' // string, regexp, array, function, or custom object
         });
     }
 
     render() {
         return(
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-header">Job description</div>
-                    <div class="form-group">
-                        <textarea onChange={this.applyHighlights} class="form-control" rows="5"></textarea>
+            <div className="card">
+                <div className="card-body">
+                    <div className="card-header">Job description</div>
+                    <div className="form-group">
+                        <textarea className="highlight form-control" onChange={this.applyHighlights} rows="5"></textarea>
                     </div>
                 </div>
             </div>
@@ -29,3 +30,89 @@ class PostDetails extends Component {
 }
 
 export default PostDetails;
+
+/*import React, { Component } from 'react';
+import $ from 'jquery';
+import './Styling/PostDetails.css';
+
+class PostDetails extends Component {
+    constructor(props) {
+        super(props);
+
+        var uaTemp = window.navigator.userAgent.toLowerCase();
+        this.state = {
+            ua: uaTemp,
+            isIE: !!uaTemp.match(/msie|trident\/7|edge/),
+            isWinPhone: uaTemp.indexOf('windows phone') !== -1,
+            isIOS: uaTemp.indexOf('windows phone') !== -1 && !!uaTemp.match(/ipad|iphone|ipod/)
+        };
+
+        this.bindEvents();
+    }
+
+    applyHighlights(text) {
+        text = text
+            .replace(/\n$/g, '\n\n')
+            .replace(/[A-Z].*?\b/g, '<mark>$&</mark>');
+
+        if (this.state.isIE) {
+            // IE wraps whitespace differently in a div vs textarea, this fixes it
+            text = text.replace(/ /g, ' <wbr>');
+        }
+
+        return text;
+    }
+
+    handleInput(event) {
+        var text = event.target.value;
+        var highlightedText = this.applyHighlights(text);
+        $('.highlights').html(highlightedText);
+    }
+
+    handleScroll() {
+        var scrollTop = $('textarea').scrollTop();
+        $('.backdrop').scrollTop(scrollTop);
+
+        var scrollLeft = $('textarea').scrollLeft();
+        $('.backdrop').scrollLeft(scrollLeft);
+    }
+
+    fixIOS() {
+        $('.highlights').css({
+            'padding-left': '+=3px',
+            'padding-right': '+=3px'
+        });
+    }
+
+    bindEvents() {
+        $('textarea').on({
+            'input': this.handleInput,
+            'scroll': this.handleScroll
+        })
+
+        var uaTemp = window.navigator.userAgent.toLowerCase();
+        if(uaTemp.indexOf('windows phone') !== -1 && !!uaTemp.match(/ipad|iphone|ipod/)) {
+            this.fixIOS();
+        }
+    }
+
+    render() {
+        return(
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-header">Job description</div>
+                    <div class="form-group">
+                        <div class="container">
+                            <div class="backdrop">
+                                <div class="highlights"></div>
+                            </div>
+                            <textarea onChange="handleInput" class="form-control" rows="5"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PostDetails;*/
